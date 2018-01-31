cda58bc00d129ae89bab74932d064901fce977fb
Tom_Vandivier <flgtom@aol.com>
2018-01-31 13:24:04 -0700

    debugging

--- rTest.js
+++ rTest.js
@@ -8,7 +8,7 @@ var top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
 // document.getElementById("mover").style.marginLeft = "-100vw";
 let aa = document.getElementsByClassName("mover");
 window.addEventListener('scroll', function(){
-    $("#about").text(window.pageYOffset);
+    document.getElementById("about").innerHtml = window.pageYOffset;
     if(window.pageYOffset > 2250 && flags[0]){
         flags[0] = true;
         flags[1] = true;
