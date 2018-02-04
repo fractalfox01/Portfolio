e73f574a19835e9ac4527b9f684bf048d9200ed6
FractalFox <flgtom@aol.com>
2018-02-03 21:58:30 -0700

    animation added to elements with ID's of proj1 - 4.

--- rTest.js
+++ rTest.js
@@ -1,8 +1,11 @@
 window.scrollTo(30,0);
-let f1 = true;
+let proj1Flag = true;
+let proj2Flag = true;
+let proj3Flag = true;
+let proj4Flag = true;
 let f2 = true;
 let f3 = true;
-let flags = [true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]
+let flags = [true, false, false, false, false, false, false, false, false, false, false, false, true, false, false, true, false, false, false, false, false]
 let progMargin = "0vw";
 let winH = window.innerHeight;
 var doc = document.documentElement;
@@ -29,9 +32,52 @@ let img = document.getElementById("image");
 let hrefProjects = document.getElementById("hrefProjects");
 let hrefLinks = document.getElementById("hrefLinks");
 let hrefUbuntu = document.getElementById("hrefUbuntu");
-let proj1 = document.getElementById("proj1").style;
 
 
+function linearEase(start, stop, percent){
+  if((start + ((stop - start) * percent))>stop){
+    return stop;
+  }else{
+    return start + ((stop - start) * percent);
+  }
+}
+
+function animateIt(options){
+  let startTime = new Date().getTime();
+  let ease = options.ease;
+  let elem = options.element;
+
+  function update(){
+    let curTime = new Date().getTime();
+    let elapsed = curTime - startTime;
+    let timeLeft = options.duration - elapsed;
+    let percent = 1 + -(timeLeft)/options.duration;
+    let move = ease(options.begin, options.left, percent);
+    console.log(ease(options.begin, options.left,percent));
+    // let elem.left = ease(-100, options.left, )
+    elem.marginLeft = move + "vw";
+    if(timeLeft > 0){
+      setTimeout(update, 33);
+    }
+  }
+  // setTimeout(update, 33);
+  update();
+}
+//
+let p1 = document.getElementById("proj1").style;
+let p2 = document.getElementById("proj2").style;
+let p3 = document.getElementById("proj3").style;
+let p4 = document.getElementById("proj4").style;
+let pos = [35, 55, 75, 95];
+
+let buttons = [p1, p2, p3, p4];
+let a = 1000;
+
+for(var i = 0; i < buttons.length; i++){
+  buttons[i].top = "10vh";
+  buttons[i].left = "-20vw";
+}
+//var elem = document.getElementById("animatable");
 
 img.addEventListener('click', function(){
     console.log('clicker');
@@ -86,6 +132,92 @@ about.addEventListener('mouseover', function(event){
 about.addEventListener('mouseout', function(event){
     event.target.style.backgroundColor = "rgb(227,227,227)";
 },false);
+
+document.getElementById("proj1").addEventListener('click', function(){
+    if(proj1Flag){
+        proj1Flag = false;
+        p1.width = "50vw";
+        p1.height = "50vh";
+        p1.marginLeft = "45vw";
+        p1.opacity = "1";
+        p2.opacity = "0";
+        p3.opacity = "0";
+        p4.opacity = "0";
+    }else{
+        proj1Flag = true;
+        p1.width = "12vw";
+        p1.height = "10vh";
+        p1.marginLeft = "32vw";
+        p1.opacity = ".65";
+        p2.opacity = ".65";
+        p3.opacity = ".65";
+        p4.opacity = ".65";
+    }
+})
+document.getElementById("proj2").addEventListener('click', function(){
+    if(proj2Flag){
+        proj2Flag = false;
+        p2.width = "50vw";
+        p2.height = "50vh";
+        p2.marginLeft = "45vw";
+        p2.opacity = "1";
+        p1.opacity = "0";
+        p3.opacity = "0";
+        p4.opacity = "0";
+    }else{
+        proj2Flag = true;
+        p2.width = "12vw";
+        p2.height = "10vh";
+        p2.marginLeft = "55vw";
+        p2.opacity = ".65";
+        p1.opacity = ".65";
+        p3.opacity = ".65";
+        p4.opacity = ".65";
+    }
+})
+document.getElementById("proj3").addEventListener('click', function(){
+    if(proj3Flag){
+        proj3Flag = false;
+        p3.width = "50vw";
+        p3.height = "50vh";
+        p3.marginLeft = "45vw";
+        p3.opacity = "1";
+        p1.opacity = "0";
+        p2.opacity = "0";
+        p4.opacity = "0";
+    }else{
+        proj3Flag = true;
+        p3.width = "12vw";
+        p3.height = "10vh";
+        p3.marginLeft = "75vw";
+        p3.opacity = ".65";
+        p1.opacity = ".65";
+        p2.opacity = ".65";
+        p4.opacity = ".65";
+    }
+})
+document.getElementById("proj4").addEventListener('click', function(){
+    if(proj4Flag){
+        proj4Flag = false;
+        p4.width = "50vw";
+        p4.height = "50vh";
+        p4.marginLeft = "45vw";
+        p4.opacity = "1";
+        p1.opacity = "0";
+        p2.opacity = "0";
+        p3.opacity = "0";
+    }else{
+        proj4Flag = true;
+        p4.width = "12vw";
+        p4.height = "10vh";
+        p4.marginLeft = "95vw";
+        p4.opacity = ".65";
+        p1.opacity = ".65";
+        p2.opacity = ".65";
+        p3.opacity = ".65";
+    }
+})
+
 window.addEventListener('scroll', function(){
     if(window.pageYOffset > 1100 && window.pageYOffset < 1195){
         for(var i = 0; i < 11; i++){
@@ -319,15 +451,17 @@ window.addEventListener('scroll', function(){
         linux.opacity = "0";
         wordpress.opacity = "0";
         python.opacity = "0";
-        flags[10] = true;
+        flags[10] = false;
         flags[11] = true;
 //        aa[9].style.marginLeft = "-100vw";
         aa[10].style.marginLeft = progMargin;
     }
-    if(window.pageYOffset > 2000 && flags[10]){
+    if(window.pageYOffset > 1900 && flags[11]){
         for(var j = 1; j < 11; j++){
             flags[j] = false;
         }
+
+        apache.zIndex = "0";
         android.opacity = "0";
         apache.opacity = "0";
         java.opacity = "0";
@@ -340,6 +474,47 @@ window.addEventListener('scroll', function(){
         wordpress.opacity = "0";
         python.opacity = "0";
 //        aa[10].style.marginLeft = "-100vw";
+
+    }
+    if((window.pageYOffset > 2100) && (window.pageYOffset < 2300) && flags[12]){
+        flags[12] = false;
+        flags[11] = true;
+        setTimeout(function(){
+          animateIt({
+            element: p4,
+            left: pos[3],
+            begin: -20,
+            duration: 2000,
+            ease: linearEase
+          })
+        },10);
+        setTimeout(function() {
+          animateIt({
+            element: p3,
+            left: pos[2],
+            begin: -20,
+            duration: 2000,
+            ease: linearEase
+          })
+        }, 20);
+        setTimeout(function() {
+          animateIt({
+            element: p2,
+            left: pos[1],
+            begin: -20,
+            duration: 2000,
+            ease: linearEase
+          })
+        }, 30);
+        setTimeout(function() {
+          animateIt({
+            element: p1,
+            left: pos[0],
+            begin: -20,
+            duration: 2000,
+            ease: linearEase
+          })
+        }, 40);
     }
 })
 console.log("working");
