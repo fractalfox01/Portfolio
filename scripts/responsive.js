29203e649df9690c33b292263ce9676c75a3da3f
FractalFox <flgtom@aol.com>
2018-03-06 22:26:52 -0700

    working on responsive(html,js,css), Buttons: testing for mobile

--- scripts/responsive.js
+++ scripts/responsive.js
@@ -2,16 +2,75 @@
   Author: Thomas Vandivier
   Description: Controls page flow */
 
-//console.log("Script Working");
+console.log("Script Working");
 //$(document).ready(function(){
 //  console.log("Jquery Working!");
 //})
-let win_Width = $.viewportW();
-let svgHeader_name = $("#frame");
-let svgHeader_about = $("#frameAbout");
 
+let svgHeader_name = $("#frame");
+let svgClickFlag_about = true;
+let svgHeader_about = $("#frameAbout");
+let svgHeader_skills = $("#frameSkills");
+let svgHeader_projects = $("#frameProjects");
+let svgHeader_links = $("#frameLinks");
+let svgHeader_education = $("#frameEducation");
 
 $(document).ready(function(){
   svgHeader_name.css("position", "fixed").css("marginTop", "10vh").css("marginLeft", "0vh").css("width", "100vw").css("height", "20vh").css("backgroundColor", "none").css("zIndex", "1");
-  console.log(win_Width);
+
+  // Svg Header Button: About -------------------------------------------
+  svgHeader_about.on('mouseover', function(){
+    svgHeader_about.css("backgroundColor", "#070").css("opacity", ".75");
+  });
+  svgHeader_about.on('mouseout',function(){
+    svgHeader_about.css("backgroundColor", "").css("opacity", "1");
+  });
+  svgHeader_about.on('click', function(){
+
+  });
+  // --------------------------------------------------------------------
+  //Svg Header Button: Skills -------------------------------------------
+  svgHeader_skills.on('mouseover', function(){
+    svgHeader_skills.css("backgroundColor", "#070").css("opacity", ".75");
+  });
+  svgHeader_skills.on('mouseout', function(){
+    svgHeader_skills.css("backgroundColor", "").css("opacity", "1");
+  });
+  svgHeader_skills.on('click', function(){
+
+  });
+  // --------------------------------------------------------------------
+  // Svg Header Button: Projects ----------------------------------------
+  svgHeader_projects.on('mouseover', function(){
+    svgHeader_projects.css("backgroundColor", "#070").css("opacity", ".75");
+  });
+  svgHeader_projects.on('mouseout', function(){
+    svgHeader_projects.css("backgroundColor", "").css("opacity", "1");
+  });
+  svgHeader_projects.on('click', function(){
+
+  });
+  // --------------------------------------------------------------------
+  // Svg Header Button: Links -------------------------------------------
+  svgHeader_links.on('mouseover', function(){
+    svgHeader_links.css("backgroundColor", "#070").css("opacity", ".75");
+  });
+  svgHeader_links.on('mouseout', function(){
+    svgHeader_links.css("backgroundColor", "").css("opacity", "1");
+  });
+  svgHeader_links.on('click', function(){
+
+  });
+  // --------------------------------------------------------------------
+  // Svg Header Button: Education ---------------------------------------
+  svgHeader_education.on('mouseover', function(){
+    svgHeader_education.css("backgroundColor", "#070").css("cpacity", ".75");
+  });
+  svgHeader_education.on('mouseout', function(){
+    svgHeader_education.css("backgroundColor", "").css("opacity", "1");
+  });
+  svgHeader_education.on('click', function(){
+
+  });
+  // --------------------------------------------------------------------
 })
