ac687f7808d8c3aa571dcbc79a28e6933d936df7
FractalFox <flgtom@aol.com>
2018-03-06 19:08:26 -0700

    working on responsive(html,js,css), updating

--- scripts/responsive.js
+++ scripts/responsive.js
@@ -6,9 +6,12 @@
 //$(document).ready(function(){
 //  console.log("Jquery Working!");
 //})
+let win_Width = $.viewportW();
+let svgHeader_name = $("#frame");
+let svgHeader_about = $("#frameAbout");
 
-let svgHeader_name = $("#frameAbout");
 
 $(document).ready(function(){
-  svgHeader_name.css("position", "fixed").css("marginTop", "10vh").css("marginLeft", "0vh").css("width", "100vw").css("height", "30vh").css("backgroundColor", "none").css("zIndex", "1");
+  svgHeader_name.css("position", "fixed").css("marginTop", "10vh").css("marginLeft", "0vh").css("width", "100vw").css("height", "20vh").css("backgroundColor", "none").css("zIndex", "1");
+  console.log(win_Width);
 })
